9e5b06fc860b49c6a4766582bc82c1eb59345030
Mike <utilisateur@CZC1289KLY.mancheopenschool.fr>
2015-11-05 16:58:50 +0100

    jeudi 05/11; TP-HTracker, petite avancée, reprendre les slide-bars dans la première tab

---
+++ TP_HTracker/script.js
@@ -0,0 +1,25 @@
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+$(function() {
+  $( "#datepicker" ).datepicker();
+});
+
+<script>
+  $(function() {
+    $( "#slider" ).slider();
+  });
+</script>
+
+<script>
+  $(function() {
+    $( "#slider" ).slider();
+  });
+</script>
+
+<script>
+  $(function() {
+    $( "#slider" ).slider();
+  });
+</script>
