336136d283e3af892eb0f806eada03b6cc252bd1
Mike <utilisateur@CZC1289KLY.mancheopenschool.fr>
2015-11-16 16:58:08 +0100

    TP HTracker, lundi 16/11, travail de groupe mené par Arthur

---
+++ Happiness tracker/site/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+	$( "#tabs" ).tabs();
+    $.datepicker.setDefaults( $.datepicker.regional[ "fr" ] );
+	    $( "#datepicker" ).datepicker({
+        altField: "#alternate",
+        altFormat: "DD, d MM, yy"
+      });
+      
+
+        $( "#slider1" ).slider({
+      range: "min",
+      min: 0,
+      max: 5,
+      value: 0,
+      slide: function( event, ui ) {
+        $( "#amount1" ).val( ui.value );
+      }
+    });
+    $( "#amount1" ).val( $( "#slider1" ).slider( "value" ) );
+
+        $( "#slider2" ).slider({
+      range: "min",
+      min: 0,
+      max: 5,
+      value: 0,
+      slide: function( event, ui ) {
+        $( "#amount2" ).val( ui.value );
+      }
+    });
+    $( "#amount2" ).val( $( "#slider2" ).slider( "value" ) );
+
+        $( "#slider3" ).slider({
+      range: "min",
+      min: 0,
+      max: 5,
+      value: 0,
+      slide: function( event, ui ) {
+        $( "#amount3" ).val( ui.value );
+      }
+    });
+    $( "#amount3" ).val( $( "#slider3" ).slider( "value" ) );
+
+});
+
