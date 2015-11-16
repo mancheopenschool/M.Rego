0c516ed67575ed53e1d42dd49d191a80362fe20e
Mike <utilisateur@CZC1289KLY.mancheopenschool.fr>
2015-11-16 12:35:57 +0100

    TP HTracker, lundi 16/11, suite

--- TP_HTracker/script.js
+++ TP_HTracker/script.js
@@ -1,25 +1,44 @@
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').focus()
-})
 
-$(function() {
-  $( "#datepicker" ).datepicker();
+$(document).ready(function(){ 
+	$('#myModal').on('shown.bs.modal', function () {
+	  	$('#myInput').focus()
+	})
+
+  	$(function() {
+    	$( "#tabs" ).tabs();
+  	});
+
+	$(function() {
+	  	$( "#datepicker" ).datepicker();
+	});
+
+	$(function() {
+	  	$( "#slider-vertical" ).slider({
+		    orientation: "vertical",
+		    range: "min",
+		    min: 0,
+		    max: 5,
+		    value: 0,
+		    slide: function( event, ui ) {
+	      		$( "#amount" ).val( ui.value );
+	    	}
+	  	});
+	  	$( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+	});
+
+	$(function() {
+	  	$( "#slider-vertical2" ).slider({
+		    orientation: "vertical",
+		    range: "min",
+		    min: 0,
+		    max: 5,
+		    value: 0,
+		    slide: function( event, ui ) {
+	      		$( "#amount2" ).val( ui.value );
+	    	}
+	  	});
+	  	$( "#amount2" ).val( $( "#slider-vertical2" ).slider( "value" ) );
+	});
+
+
 });
-
-<script>
-  $(function() {
-    $( "#slider" ).slider();
-  });
-</script>
-
-<script>
-  $(function() {
-    $( "#slider" ).slider();
-  });
-</script>
-
-<script>
-  $(function() {
-    $( "#slider" ).slider();
-  });
-</script>
