35f2c07873547414e1f719fdb7e93175c6e20769
Mike <utilisateur@CZC1289KLY.mancheopenschool.fr>
2015-11-19 16:58:51 +0100

    Happiness tracker (projet groupe) continué, tentative presque réussie d'affichage de graph (responsivity à revoir)

--- Happiness tracker/site/script.js
+++ Happiness tracker/site/script.js
@@ -40,61 +40,6 @@ $(document).ready(function(){
     });
     $( "#amount3" ).val( $( "#slider3" ).slider( "value" ) );
 
-
-
-            // prepare chart data
-            var  sampleData = [
-                    { Day:'Monday', Keith:30, Erica:15, George: 25},
-                    { Day:'Tuesday', Keith:25, Erica:25, George: 30},
-                    { Day:'Wednesday', Keith:30, Erica:20, George: 25},
-                    { Day:'Thursday', Keith:35, Erica:25, George: 45},
-                    { Day:'Friday', Keith:20, Erica:20, George: 25},
-                    { Day:'Saturday', Keith:30, Erica:20, George: 30},
-                    { Day:'Sunday', Keith:60, Erica:45, George: 90}
-                ];
-            
-            // prepare jqxChart settings
-            var settings = {
-                title: "Fitness & exercise weekly scorecard",
-                description: "Time spent in vigorous exercise",
-                padding: { left: 5, top: 5, right: 5, bottom: 5 },
-                titlePadding: { left: 90, top: 0, right: 0, bottom: 10 },
-                source: sampleData,
-                categoryAxis:
-                    {
-                        dataField: 'Day',
-                        showGridLines: false
-                    },
-                colorScheme: 'scheme01',
-                seriesGroups:
-                    [
-                        {
-                            type: 'column',
-                            columnsGapPercent: 30,
-                            seriesGapPercent: 0,
-                            valueAxis:
-                            {
-                                minValue: 0,
-                                maxValue: 100,
-                                unitInterval: 10,
-                                description: 'Time in minutes'
-                            },
-                            series: [
-                                    { dataField: 'Keith', displayText: 'Keith'},
-                                    { dataField: 'Erica', displayText: 'Erica'},
-                                    { dataField: 'George', displayText: 'George'}
-                                ]
-                        }
-                    ]
-            };
-            
-            // select the chartContainer DIV element and render the chart.
-            $('#chartContainer').jqxChart(settings);
-
-
-
-
-
 });
 
 
