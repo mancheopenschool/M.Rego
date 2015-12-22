b7329d54b810930b76ff3cc9d998e7c76b33f556
Mike <utilisateur@CZC1289KLY.mancheopenschool.fr>
2015-12-22 17:00:34 +0100

    save de tout /html

---
+++ TP/TP Quizz/script.js
@@ -0,0 +1,23 @@
+$(document).ready() {
+	$(function() {
+    	$("#radio").buttonset();
+  	});
+
+	var question = ["Where is Brian ?", "Who is Harry Potter ?", "What is your quest ?"]
+	var r1 = ["Brian is in the bathroom", "He's a wizard", "To seek for a hamburger !" ]
+	var r2 = ["Brian is in the kitchen", "He's a moogle", "Too sick to answer that !"]
+	var r3 = ["Brian is in the brothel", "He's a ghost", "To seek the Holy Grail !"]
+	var bonne_reponse = [2, 1, 3]
+	var reponse_utilisateur = [0]
+
+	function.changer_question(
+		)
+
+
+
+	$(".btn-primary").onclick() {
+
+	};
+
+
+};
